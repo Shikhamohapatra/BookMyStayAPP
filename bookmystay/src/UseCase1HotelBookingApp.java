c2c54355c61576f7eb81643f22c90859b36e2dab
Shikhamohapatra <shikhamohapatra204@gmail.com>
2026-04-08 09:57:51 +0530

    book my stay

---
+++ bookmystay/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,18 @@
+
+public class UseCase1HotelBookingApp {
+
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("===================================");
+        System.out.println("   Welcome to Hotel Booking App   ");
+        System.out.println("===================================");
+
+        // Application details
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: v1.0");
+
+        // End message
+        System.out.println("Application started successfully!");
+    }
+}
